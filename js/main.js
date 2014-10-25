cea7b109521483bc05be6c37eb59f51dc15fa84a
rayhan_v@me.com <rayhan_v@me.com>
2014-10-25 18:04:43 -0400

    tick functionality added

--- js/main.js
+++ js/main.js
@@ -4,24 +4,52 @@ var textBox = $('input[type="text"]');
 
 textBox.on("keyup", function(e){
 	if (e.keyCode === 13) {
-		currentBox = textBox.index(this);
-		if (textBox[currentBox + 1] != null){
-		nextBox = textBox[currentBox + 1];	
+		var currentBox = textBox.index(this);
+		if (textBox[currentBox + 1] !== null){
+		var nextBox = textBox[currentBox + 1];	
 		nextBox.focus();
         nextBox.select();
         event.preventDefault();
         return false; 		
-		}
+		} 
+	}
+	// var lastBox = textBox.length;
+	// console.log(lastBox)
+	// if (nextBox = lastBox){
+	// lastBox.blur();
+	// }
+});
+
+$("input.first").on("blur", function(){ //when leaving input, add tick mark 
+	if ($("input.first").val() !== "") {
+		$("img.task1Img").fadeIn(1100);
+	} else {
+		$("img.task1Img").fadeOut(800);
+		
 	}
 });
 
-// textbox.blur() //when leaving input, add tick mark 
-	// (if textbox != null) {
-	// 	show tick 
-	// }
+$("input.second").on("blur", function(){ //when leaving input, add tick mark 
+	if ($("input.second").val() !== "") {
+		$("img.task2Img").fadeIn(1200);
+	} else {
+		$("img.task2Img").fadeOut(900);
+		
+	}
+});
+
+$("input.three").on("blur", function(){ //when leaving input, add tick mark 
+	if ($("input.third").val() !== "") {
+		$("img.task3Img").fadeIn(1200);
+	} else {
+		$("img.task3Img").fadeOut(900);
+		
+	}
+});
+
 $("h1.arrow").click(function(){
 	$('html,body').animate({scrollTop:500});	
-})
+});
 
 }); //document ready
 
