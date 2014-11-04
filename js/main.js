7b14b3d481c3b10800ebdafdee80b432622b0e62
rayhan_v@me.com <rayhan_v@me.com>
2014-11-03 21:18:39 -0500

    storage, all three click

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ textBox.on("keyup", function(e){
 // Date and time on top
 
 var today = new Date();
-
+var count = 0;
 var date = today.getDate();
 var month = new Array();
 month[0] = "January";
@@ -66,41 +66,50 @@ var tick1 = $("img.task1Img");
 var tick2 = $("img.task2Img");
 var tick3 = $("img.task3Img");
 
+var $input1 = $("input.first");
+var $input2 = $("input.second");
+var $input3 = $("input.third");
+
 //Tick animation
-$("input.first").on("blur", function(){ //when leaving input, add tick mark 
-	if ($("input.first").val() !== "") {
+$input1.on("blur", function(){ //when leaving input, add tick mark 
+	if ($input1.val() !== "") {
 		tick1.fadeIn(1100);
 	} else {
 		tick1.fadeOut(800);	
 	}
 });
 
-$("input.second").on("blur", function(){ //when leaving input, add tick mark 
-	if ($("input.second").val() !== "") {
-		$("img.task2Img").fadeIn(1200);
+$input2.on("blur", function(){ //when leaving input, add tick mark 
+	if ($input2.val() !== "") {
+		tick2.fadeIn(1200);
 	} else {
-		$("img.task2Img").fadeOut(900);
+		tick2.fadeOut(900);
 		
 	}
 });
 
-$("input.third").on("blur", function(){ //when leaving input, add tick mark 
-	if ($("input.third").val() !== "") {
-		$("img.task3Img").fadeIn(1200);
+$input3.on("blur", function(){ //when leaving input, add tick mark 
+	if ($input3.val() !== "") {
+		tick3.fadeIn(1200);
 	} else {
-		$("img.task3Img").fadeOut(900);
+		tick3.fadeOut(900);
 		
 	}
 });
 //Adding tasks to completed tasks
 tick1.on("click", function(){
-	$("ul.complete").append("<li>" + $("input.first").val() + "</li>");
+	var todo = $("input.first").val();
+	$("ul.complete").append("<li>" + todo + "</li>");
 	//Strikethrough completed tasks
   	$('#line1').animateStrikethrough(1200, function () {
   	});
 	tick1.fadeOut(500);
 	$(".close1").fadeIn(900);
-	$("input.first").addClass("hide");
+	$input1.addClass("hide");
+	count++;
+	showVideo(); // show video?
+
+	localStorage.completed = localStorage.completed + "," + todo;
 });
 
 
@@ -110,7 +119,9 @@ tick2.on("click", function(){
   	});
 	tick2.fadeOut(500);
 	$(".close2").fadeIn(900);
-	$("input.second").addClass("hide");
+	$input2.addClass("hide");
+	count++;
+	showVideo(); // show video?
 });
 
 tick3.on("click", function(){
@@ -119,32 +130,37 @@ tick3.on("click", function(){
   	});
 	tick3.fadeOut(500);
 	$(".close3").fadeIn(900);
-	$("input.third").addClass("hide");
+	$input3.addClass("hide");
+	count++;
+	showVideo(); // show video?
 });
 
 //Clearing feilds on x click
 $(".close1").on("click", function(){
 	$(this).fadeOut(600);
-	$("input.first").val('');
-	$("input.first").removeClass("hide");
+	$input1.val('').removeClass("hide");
 });
 
 $(".close2").on("click", function(){
 	$(this).fadeOut(600);
-	$("input.second").val('');
-	$("input.second").removeClass("hide");
+	$input2.val('').removeClass("hide");
 });
 
 $(".close3").on("click", function(){
 	$(this).fadeOut(600);
-	$("input.third").val('');
-	$("input.third").removeClass("hide");
+	$input3.val('').removeClass("hide");
 });
 
 //Scroll down on arrow click
 $(".scroll").click(function(){
 	$('html,body').animate({scrollTop:450});	
 });
+
+var showVideo = function() {
+	if(count >= 3) {
+		$(".overlay").addClass("slide");
+	}
+}
 
 // tick1.on("click", function(){
 // $.jStorage.set('key', $("input.first").val());
@@ -155,10 +171,16 @@ $(".scroll").click(function(){
 
 //video
 
-if ($("input.first, input.second, input.third").click()){
-	$(".video").addClass("slide");
-}
+// if ( tick1 && tick2, tick3).click() ){
+// 	console.log ("click")
+// 	$(".video").addClass("slide");
+// }
 
+	var completed = localStorage.completed.split(',');
+
+	for (var i = 0; i < completed.length; i++) {
+		$('ul.complete').append('<li>'+completed[i]+'</li>');
+	};
 
 }); //document ready
 
